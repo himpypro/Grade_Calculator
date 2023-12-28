b044b7e0b8a08d5a8caf34157f695dec99245332
himpypro <himadri.baidya507@gmail.com>
2023-12-28 12:59:26 +0600

    To calculate marks and define the grade.

---
+++ Grade_Calculator.py
@@ -0,0 +1,24 @@
+print("Grade Calculator")
+Sub_1 = int(input("Put your score here: "))
+Sub_2 = int(input("Put your score here: "))
+Sub_3 = int(input("Put your score here: "))
+Sub_4 = int(input("Put your score here: "))
+Sub_5 = int(input("Put your score here: "))
+Sub_t= (Sub_1+Sub_2+Sub_3+Sub_4+Sub_5)
+print("=> Accumulated score: ",Sub_t)
+score = Sub_t/5
+print("=> Mean score is ",score)
+if score in range(80, 100):                  #Used If statement with for loop in range.
+    print("***You got A+ <Outstanding>***")
+elif score in range(70, 80):
+    print("***You got A <Excellent>***")
+elif score in range(60, 70):
+    print("***You got A- <Very Good>***")
+elif score in range(50, 60):
+    print("***You got B <Good>***")
+elif score in range(40, 50):
+    print("***You got C <Average>***")
+elif score in range(33, 40):
+    print("***You got D <Pass>***")
+else:
+    print("Fail <Good Luck>")
